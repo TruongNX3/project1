ecb1148575c0be98db2d7416bf82f482ae441717
TruongDepZai <nguyenxuantruong96.hust@gmail.com>
2018-07-26 10:07:02 +0700

    areahcn

---
+++ areahcn.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class areahcn {
+    public static void main(String[] args) {
+        System.out.println("Nhap vao chieu rong: ");
+        Scanner sc=new Scanner(System.in);
+        float width;
+        width=sc.nextFloat();
+        System.out.println("Nhap vao chieu cao");
+        float height;
+        height=sc.nextFloat();
+        float area;
+        if(width<height){
+            System.out.println("Ban da nhap sai roi!");
+        } else{
+            area=width*height;
+            System.out.println("Dien tich hinh chu nhat la: "+ area);
+        }
+    }
+}
+
